168f666df0c88f9f2e0f953ef0809dc11a68ce65
sumukhamh <sumukhamh@gmail.com>
2019-02-13 13:05:52 -0800

    changed service type

--- multiagent_planning/src/planner.cpp
+++ multiagent_planning/src/planner.cpp
@@ -5,3 +5,13 @@
 #include "multiagent_planning/Pose.h"
 #include "std_msgs/String.h"
 
+bool get_plan_func(){}
+
+int main(int argc, char const *argv[]){
+
+	ros::init(argc, argv, "planner");
+  	ros::NodeHandle n("~");
+
+	ros::ServiceServer get_plan_server = n.advertiseService("/get_plan", get_plan_func);
+	return 0;
+}
